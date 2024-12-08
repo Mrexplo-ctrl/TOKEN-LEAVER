70d4a0dd5b9267bb53d4078f44d6b13d08cb6e62
ER DUCE <cicciosocornetto@gmail.com>
2024-12-08 18:03:38 +0100

    Create start.cmd

---
+++ start.cmd
@@ -0,0 +1,11 @@
+@echo off
+if not exist venv (
+    echo Creating virtual environment...
+    python -m venv venv
+)
+echo Activating virtual environment...
+call venv\Scripts\activate.bat
+echo Installing dependencies from requirements.txt...
+pip install -r requirements.txt
+py Main.py
+pause
